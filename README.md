d4c43ea4554d14526150be565b90e2799b0b51f7
Grundmanis <grundmanweb@gmail.com>
2023-07-20 12:45:50 +0300

    Testing (#20)
    * updated commit
    
    * readme upd
    
    * readme upd
    
    * readme upd
    
    * firefox

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ readme1
 readme1
 added new line
 added new line
+readme1
+added new line
--- dist/firefox/index.js
+++ dist/firefox/index.js
@@ -2,4 +2,5 @@ console.log("firefox");
 console.log("firefox");
 console.log("firefox");
 console.log("firefox");
+console.log("firefox");
 
